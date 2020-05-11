8fe0303934c246e724052c55cf9ee6ffc17dc2bd
Kartheek Munaga <kartheekmunaga@gmail.com>
2020-05-11 08:01:44 +0000

    deploy.sh change

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /home/jenkins/jenkinslatest/centos/remote-key /tmp/.auth prod-user@jenkins-testing-remote.eastus.cloudapp.azure.com:/tmp/.auth
+scp -i /opt/remote-key /tmp/.auth prod-user@jenkins-testing-remote.eastus.cloudapp.azure.com:/tmp/.auth
 #scp -i /opt/prod ./jenkins/deploy/publish prod-user@jenkins-testing-remote.eastus.cloudapp.azure.com:/tmp/publish
 #ssh -i /opt/prod prod-user@jenkins-testing-remote.eastus.cloudapp.azure.com"/tmp/publish"
