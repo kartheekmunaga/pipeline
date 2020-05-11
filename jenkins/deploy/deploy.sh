5c616f7b97be08bbe364f93482c62c8295f7ea78
Kartheek Munaga <kartheekmunaga@gmail.com>
2020-05-11 08:10:30 +0000

    deploy.sh change

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,5 +5,5 @@ echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
 scp -i /opt/remote-key /tmp/.auth prod-user@jenkins-testing-remote.eastus.cloudapp.azure.com:/tmp/.auth
-#scp -i /opt/prod ./jenkins/deploy/publish prod-user@jenkins-testing-remote.eastus.cloudapp.azure.com:/tmp/publish
-#ssh -i /opt/prod prod-user@jenkins-testing-remote.eastus.cloudapp.azure.com"/tmp/publish"
+scp -i /opt/prod ./jenkins/deploy/publish prod-user@jenkins-testing-remote.eastus.cloudapp.azure.com:/tmp/publish
+ssh -i /opt/prod prod-user@jenkins-testing-remote.eastus.cloudapp.azure.com"/tmp/publish"
